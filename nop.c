f8b8428f4ba0b83333da63da57b33cb55c10a183
“cliveclistar” <clivemadungwe281@gmail.com>
2023-12-15 03:33:05 -0800

    new update

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  * the f_nop- nothing
+  * the @head: stack head
+  * the @counter: line_number
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
